3afee0d0adff3eec31351a4f0342d1fd6f379f07
Malychee <98172766+Malychee@users.noreply.github.com>
2022-04-23 11:04:34 -0400

    Add files via upload

--- WarGamePlayer.java
+++ WarGamePlayer.java
@@ -1,19 +1,46 @@
+import java.util.function.ToDoubleBiFunction;
+
+import javafx.scene.control.TableFocusModel;
+
 public class WarGamePlayer {
     private String name;
     private GroupOfCards playerDeck;
 
     public WarGamePlayer(String name) {
-        this.name = name;
+
     }
 
     public String getName() {
         return this.name;
     }
 
-    public void setName(){
-        
+    public void setName(String name){
+        this.name = name;
     }
 
+    public GroupOfCards getPlayerDeck() {
+        return playerDeck;
+    }
+
+    public void setPlayerDeck(GroupOfCards playerDeck) {
+        this.playerDeck = playerDeck;
+    }
+
+    //Complete when other classes are done
+
+    public void removeCards(){
+
+    }
+
+    public void addCards() {
+
+    }
+
+    public boolean checkDeckEmpty() {
+
+    }
+
+    public String getTopCard() { 
 
     
 }
