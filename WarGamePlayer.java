5d203dee0b246036db9850eb3717269b58327172
Malychee <98172766+Malychee@users.noreply.github.com>
2022-04-23 11:03:01 -0400

    Add files via upload
    Still needs addition of other methods from other classes

---
+++ WarGamePlayer.java
@@ -0,0 +1,19 @@
+public class WarGamePlayer {
+    private String name;
+    private GroupOfCards playerDeck;
+
+    public WarGamePlayer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(){
+        
+    }
+
+
+    
+}
